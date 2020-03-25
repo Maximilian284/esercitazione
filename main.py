af87be0a15698050cbbe0d1bbd80bcc647221950
Massimiliano Prandi <maxallep@gmail.com>
2020-03-25 15:00:16 +0100

    modified main.py

--- main.py
+++ main.py
@@ -18,12 +18,32 @@ class Editor():
 
     # Text Auto Resizable
     self.__text = Text(self.__window)
-    self.__window.grid_rowconfigure(0, weight=1) 
+    self.__scrollbar = Scrollbar(self.__text)
+    self.__window.grid_rowconfigure(0, weight=1)
     self.__window.grid_columnconfigure(0, weight=1)
 
     # Fill The Window With The Text
     self.__text.grid(sticky = N + E + S + W)
 
+    # Set Menu
+    self.__menu_bar = Menu(self.__window)
+    self.__file_menu = Menu(self.__menu_bar, tearoff = 0)
+
+    # File Menu
+    self.__file_menu.add_command(label = "Esci", command = self.__exit, accelerator = "Ctrl+Q")
+
+    self.__window.bind("<Control-q>", self.__exit)
+
+    self.__menu_bar.add_cascade(label = "File", menu = self.__file_menu)
+
+    # Add Menus
+    self.__window.config(menu = self.__menu_bar)
+    self.__scrollbar.pack(side = RIGHT, fill = Y)
+
+  # File Menu's Commands
+      # close the window
+  def __exit(self, __event = None):
+    self.__window.destroy()
 
   # Run Editor
       # start the main loop of the window
